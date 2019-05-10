70617b8a68ab5e78df819bde76afcc7d52d0fa95
Michael E Uhl <michael.uhl@gmail.com>
2019-05-10 19:15:44 -0400

    Initial work on the Smart Home message interface.

---
+++ src/lambdaskill2/smarthome/messages.py
@@ -0,0 +1,214 @@
+from uuid import uuid4
+
+from lambdaskill2.enums import StrEnum
+
+
+class ScopeType(StrEnum):
+    BEARER_TOKEN = "BearerToken"
+    BEARER_TOKEN_WITH_PARTITION = "BearerTokenWithPartition"
+
+
+class Scope(object):
+
+    def __init__(self, type_, token, partition=None, user_id=None):
+        self.__type = ScopeType(type_)
+        self.__token = token
+        if self.__type is ScopeType.BEARER_TOKEN_WITH_PARTITION:
+            if partition is None or user_id is None:
+                raise ValueError('partition and user_id are required for BEARER_TOKEN_WITH_PARTITION')
+        self.__partition = partition
+        self.__user_id = user_id
+
+    @property
+    def type(self):
+        return self.__type
+
+    @property
+    def token(self):
+        return self.__token
+
+    @property
+    def partition(self):
+        if self.__type is ScopeType.BEARER_TOKEN:
+            raise TypeError('BEARER_TOKEN-type scopes do not support the "partition" attribute.')
+        return self.__partition
+
+    @property
+    def user_id(self):
+        if self.__type is ScopeType.BEARER_TOKEN:
+            raise TypeError('BEARER_TOKEN-type scopes do not support the "user_id" attribute.')
+        return self.__user_id
+
+    @classmethod
+    def from_json(cls, json):
+        if json is None:
+            return None
+        return cls(type_=json['type'],
+                   token=json['token'],
+                   partition=json.get('partition', None),
+                   user_id=json.get('userId', None))
+
+    def prepare(self):
+        container = {
+            'type': self.__type,
+            'token': self.__token
+            }
+        if self.__type is ScopeType.BEARER_TOKEN_WITH_PARTITION:
+            container['partition'] = self.__partition
+            container['userId'] = self.__user_id
+        return container
+
+
+class EndPoint(object):
+
+    def __init__(self, scope, endpoint_id, cookie=None):
+        self.__scope = scope
+        self.__endpoint_id = endpoint_id
+        self.__cookie = cookie if cookie is not None else {}
+
+    @property
+    def scope(self):
+        return self.__scope
+
+    @property
+    def endpoint_id(self):
+        return self.__endpoint_id
+
+    @property
+    def cookie(self):
+        return self.__cookie
+
+    @classmethod
+    def from_json(cls, json):
+        if json is None:
+            return None
+        return cls(scope=Scope.from_json(json['scope']),
+                   endpoint_id=json['endpointId'],
+                   cookie=json['cookie'])
+
+    def prepare(self):
+        return {
+            'scope': self.__scope.prepare(),
+            'endpointId': self.__endpoint_id,
+            'cookie': self.__cookie
+            }
+
+
+class Header(object):
+
+    def __init__(self, namespace, name, payload_version, message_id=None, correlation_token=None):
+        self.__namespace = namespace
+        self.__name = name
+        self.__payload_version = payload_version
+        self.__message_id = message_id
+        if not self.__message_id:
+            self.__message_id = str(uuid4())
+        self.__correlation_token = correlation_token
+
+    @property
+    def namespace(self):
+        return self.__namespace
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def payload_version(self):
+        return self.__payload_version
+
+    @property
+    def message_id(self):
+        return self.__message_id
+
+    @property
+    def correlation_token(self):
+        return self.__correlation_token
+
+    @classmethod
+    def from_json(cls, json):
+        if json is None:
+            return None
+        return cls(namespace=json['namespace'],
+                   name=json['name'],
+                   message_id=json['messageId'],
+                   correlation_token=json.get('correlationToken', None),
+                   payload_version=json['payloadVersion'])
+
+    def prepare(self):
+        container = {
+            'namespace': self.__namespace,
+            'name': self.__name,
+            'payloadVersion': self.__payload_version,
+            'messageId': self.__message_id
+            }
+        if self.__correlation_token is not None:
+            container['correlationToken'] = self.__correlation_token
+        return container
+
+
+class Message(object):
+
+    def __init__(self, header, endpoint, payload):
+        self.__header = header
+        self.__endpoint = endpoint
+        self.__payload = payload if payload else {}
+
+    @property
+    def header(self):
+        return self.__header
+
+    @property
+    def endpoint(self):
+        return self.__endpoint
+
+    @property
+    def payload(self):
+        return self.__payload
+
+    @classmethod
+    def from_json(cls, json):
+        if json is None:
+            return None
+        return cls(header=Header.from_json(json.get('header', None)),
+                   endpoint=EndPoint.from_json(json.get('endpoint', None)),
+                   payload=None)
+
+    def prepare(self):
+        container = {}
+        if self.__header is not None:
+            container['header'] = self.__header.prepare()
+        if self.__endpoint is not None:
+            container['endpoint'] = self.__endpoint.prepare()
+        if self.__payload is not None:
+            container['payload'] = self.__payload
+        return container
+
+
+class Event(Message):
+
+    def __init__(self, header, endpoint, payload=None, context=None):
+        if payload is None:
+            payload = {}
+        super(Event, self).__init__(header=header,
+                                    endpoint=endpoint,
+                                    payload=payload)
+        self.__context = context
+
+    @property
+    def context(self):
+        return self.__context
+
+    def with_payload(self, payload):
+        self.__payload = payload
+        return self
+
+    def with_context(self, context):
+        self.__context = context
+        return self
+
+    def prepare(self):
+        container = super(Event, self).prepare()
+        if self.__context is not None:
+            container['context'] = self.__context
+        return container
---
+++ tests/integration/smarthome/test_messages.py
@@ -0,0 +1,53 @@
+import json
+import unittest
+
+from lambdaskill2.smarthome.messages import *
+
+
+class MessageDirectiveTestCase(unittest.TestCase):
+
+    def setUp(self):
+        with open('tests/integration/data/message-directive-turn_on-with_partition.json', 'rt') as jsonfile:
+            self.json = json.load(jsonfile)
+
+    def test_header(self):
+        header = Header.from_json(self.json['directive']['header'])
+        self.assertEqual(header.namespace, "Alexa.PowerController")
+        self.assertEqual(header.name, "TurnOn")
+        self.assertEqual(header.message_id, "message-id")
+        self.assertEqual(header.correlation_token, "correlation-token")
+        self.assertEqual(header.payload_version, "3")
+
+    def test_scope_with_partition(self):
+        scope = Scope.from_json(self.json['directive']['endpoint']['scope'])
+        self.assertIs(scope.type, ScopeType.BEARER_TOKEN_WITH_PARTITION)
+        self.assertEqual(scope.token, "access-token")
+        self.assertEqual(scope.partition, "partition-id")
+        self.assertEqual(scope.user_id, "user-id")
+
+    def test_endpoint(self):
+        endpoint = EndPoint.from_json(self.json['directive']['endpoint'])
+        self.assertIsInstance(endpoint.scope, Scope)
+        self.assertEqual(endpoint.endpoint_id, "endpoint-id")
+        self.assertDictEqual(endpoint.cookie, {"key-1": "value-1",
+                                               "key-0": "value-0"})
+
+    def test_directive(self):
+        directive = Message.from_json(self.json['directive'])
+        self.assertIsInstance(directive.header, Header)
+        self.assertIsInstance(directive.endpoint, EndPoint)
+        self.assertDictEqual(directive.payload, {})
+
+
+class BearerTokenScopeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        with open('tests/integration/data/message-directive-turn_on.json', 'rt') as jsonfile:
+            self.json = json.load(jsonfile)
+
+    def test_scope(self):
+        scope = Scope.from_json(self.json['directive']['endpoint']['scope'])
+        self.assertIs(scope.type, ScopeType.BEARER_TOKEN)
+        self.assertEqual(scope.token, "access-token")
+        self.assertRaises(TypeError, getattr, scope, "partition")
+        self.assertRaises(TypeError, getattr, scope, "user_id")
